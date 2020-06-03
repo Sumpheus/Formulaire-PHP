d00f379236a3cfe6e3fe6bf64a206c20ed7a632c
Kevin 'Sumpheus' Nguma <51791578+Sumpheus@users.noreply.github.com>
2020-06-03 16:17:08 +0200

    PHP form, not modified with js and css yet

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href='style.css' rel="stylesheet"/>
+    <title></title>
+  </head>
+  <body>
+  <form class="" action="results.php" method="post">
+    <label for 'nom'> Veullez entrer votre nom:</label>
+    <input type='text' name='nom' placeholder='Name' id='Nom'>
+    <label for='prenom'>Veuillez entrer votre prénom:</label>
+    <input type='text' name='prenom' placeholder=First-Name id='Prenom'>
+    <input type='submit' name='' value='Envoyer'>
+  </form>
+
+  <?php
+
+
+
+   ?>
+
+  </body>
+</html>
---
+++ results.php
@@ -0,0 +1,7 @@
+<?php
+
+  $nom = $_POST["nom"];
+  $prenom = $_POST["prenom"];
+  echo "The best boxer of all time is" . $prenom  . $nom;
+
+ ?>
